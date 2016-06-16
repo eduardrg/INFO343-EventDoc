5ea4ebc632a2e33f8c88ea5b1711693feae4c536
Eduard <eduardrg@uw.edu>
2016-06-16 04:02:50 -0700

    trying to fix back to top link

--- js/app.js
+++ js/app.js
@@ -28,6 +28,9 @@ myApp.config(function($routeProvider) {
       templateUrl: 'templates/soon.html',
       controller: 'SoonController',
     })
+    .otherwise({
+      redirectTo: '/'
+    })
 })
    // Landing page controller
 .controller('LandingController', function($scope){
@@ -76,3 +79,48 @@ myApp.config(function($routeProvider) {
 .controller('SoonController', function($scope){
   $scope.about = "Here's some information about this page."
 })
+        // SHAMELESSLY COPIED AND PASTED FROM
+        // www.bennadel.com/blog/2869-using-anchor-tags-and-url-fragment-links-in-angularjs.htm
+
+        // I watch the ngAnchor attribute to automatically configure the HREF value to
+        // use natural URL-fragment behavior in AngularJS.
+        // --
+        // NOTE: We don't actually need the $anchorScroll() service; but, we need to
+        // inject it here in order to guarantee that it is instantiated and starts
+        // watching the $location for changes.
+.directive(
+    "ngAnchor",
+    function anchorDirective( $location, $anchorScroll ) {
+        // Return the directive configuration object.
+        return({
+            link: link,
+            restrict: "A"
+        });
+        // I bind the JavaScript events to the view-model.
+        function link( scope, element, attributes ) {
+            // Whenever the attribute changes, we have to update our HREF state
+            // to incorporate the new ngAnchor value as the embedded fragment.
+            attributes.$observe( "ngAnchor", configureHref );
+            // Whenever the the location changes, we want to update the HREF value
+            // of this link to incorporate the current URL plus the URL-fragment
+            // that we are watching in the ngAnchor attribute.
+            scope.$on( "$locationChangeSuccess", configureHref );
+            // I update the HREF attribute to incorporate both the current top-
+            // level fragment plus our in-page URL-fragment intent.
+            function configureHref() {
+                var fragment = ( attributes.ngAnchor || "" );
+                // Strip off the leading # to make the string concatenation
+                // handle variable-state inputs (ie, ones that may or may not
+                // include the leading pound sign).
+                if ( fragment.charAt( 0 ) === "#" ) {
+                    fragment = fragment.slice( 1 );
+                }
+                // Since the anchor is really the fragment INSIDE the fragment,
+                // we have to build two levels of fragment.
+                var routeValue = ( "#" + $location.url().split( "#" ).shift() );
+                var fragmentValue = ( "#" + fragment );
+                attributes.$set( "href", ( routeValue + fragmentValue ) );
+            }
+        }
+    }
+)
